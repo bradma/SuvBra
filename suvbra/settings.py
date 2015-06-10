8d48dabfcfb56ac987493d364c5e14c589f00633
twinsen43 <twinsen9@hotmail.com>
2015-06-10 19:43:47 -0400

    initial commit

---
+++ suvbra/settings.py
@@ -0,0 +1,81 @@
+# Project Suvbra
+import os
+from os.path import join
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+TEMPLATE_DIRS = (
+	join(BASE_DIR, 'templates')
+)
+
+STATICFILES_DIRS = (
+	join(BASE_DIR, 'media')
+)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'n(_^$$ljl8w63kk=ql#gdv_!#tr5(ersw(o0ueyqc$w1sz@n$s'
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+DJANGO_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+LOCAL_APPS = (
+	
+)
+
+THIRD_PARTY_APPS = (
+
+)
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'suvbra.urls'
+
+WSGI_APPLICATION = 'suvbra.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.mongodb_engine',
+        'NAME': '',,
+	'USER' : '',
+	'PASSWORD' : '',
+	'HOST' : '',
+	'PORT' : '',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
---
+++ suvbra/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for suvbra project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "suvbra.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
