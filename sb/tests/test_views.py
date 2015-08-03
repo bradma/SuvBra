10fe9908c3dd0366040731766de482d794c62957
twinsen43 <twinsen9@hotmail.com>
2015-08-03 00:31:32 +0000

    TODO: Fix broken tests

--- sb/tests/test_views.py
+++ sb/tests/test_views.py
@@ -1,11 +1,31 @@
-from django.test import SimpleTestCase, RequestFactory
+from django.test import TestCase, RequestFactory
 from django.core.urlresolvers import reverse
 
-from sb.views import first_view
+from sb.views import first_view, customer_detail
 
-class test_my_views(SimpleTestCase):
+from sb.models import customer
+
+class test_my_views(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+        customer.objects.create(
+            first_name= 'Test',
+            last_name= 'Me',
+            home_phone= '2549583453',
+            email = 'x@skip.com',
+            full_address = '320 Charlies Lane',
+            city = 'Philly',
+            state = 'PA',
+            zip_code = '39434'
+        )
+
+    def test_customer_detail__status_code(self):
+        """ Test that customer detail is status 200 """
+
+        url = reverse('test:customer-detail', kwargs={'pk': 1})
+        request = self.factory.get(url)
+        response = customer_detail.as_view()(request)
+        self.assertEqual(response.status_code, 200)
 
     def test_first_view__status_code(self):
         """ Test that Concept view is rendering """
@@ -26,7 +46,6 @@ class test_my_views(SimpleTestCase):
                 response.context_data[test_context]
         except KeyError as ex:
             self.fail(ex)
-
 
     def test_first_view__template_correct(self):
         """ Test view is looking at correct template """
