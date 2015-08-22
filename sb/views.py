cb4e6e439e9235b27ea99d7f9e958c38e725a782
twinsen43 <twinsen9@hotmail.com>
2015-08-22 17:31:58 +0000

    Adding part of login view logic

--- sb/views.py
+++ sb/views.py
@@ -4,7 +4,47 @@ from django.views.generic import View, DetailView
 
 from sb.serializers import CustSerializer
 from sb.models import customer
-from sb.forms import customer_form
+from sb.forms import customer_form, login_form
+
+class login_view(View):
+    template_name = 'login/login.html'
+    form_class = login_form
+
+    def get(self, request):
+        context = {'form': self.form_class()}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        print ('Pass for Now')
+
+"""
+class main_login(View):
+    template_name = 'login.html'
+    form_class = my_login
+
+    def get(self, request, *args, **kwargs):
+        context = {'form': self.form_class()}
+        return render(request, self.template_name, context)
+
+    def post (self, request, *args, **kwargs):
+        context = {'form': self.form_class()}
+        form = self.form_class(self.request.POST or None)
+        if form.is_valid():
+            _username = request.POST['username']
+            _password = request.POST['password']
+            userlogin = authenticate(username=_username, password=_password)
+            if userlogin:
+                if userlogin.is_active:
+                    login(request, userlogin)
+                    return HttpResponseRedirect(reverse('main'))
+                else:
+                    return HttpResponse('Invalid')
+            else:
+                return HttpResponse('Invalid Username or Password')
+        return render(request, self.template_name, context)
+"""
+
+
 
 class first_view(View):
     template_name = 'suvbra/home.html'
