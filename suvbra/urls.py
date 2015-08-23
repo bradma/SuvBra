f9a849f660fdf6e93bb3bb7622ac480e51982f0b
twinsen43 <twinsen9@hotmail.com>
2015-08-23 17:14:04 +0000

    Tweaking settings for reroute and reorganizing the urls

--- suvbra/urls.py
+++ suvbra/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
+from django.contrib import admin
 
-from sb.views import cust_viewset
+from sb.views import cust_viewset, login_view
 
 router = routers.DefaultRouter()
 router.register(r'customers', cust_viewset)
 
 urlpatterns = patterns('',
+    url(r'^$', login_view.as_view(), name='login'),
 	url(r'^Concept/', include('sb.urls', namespace='test')),
     url(r'^api/', include(router.urls, namespace='rest_api')),
+    url(r'^admin/', include(admin.site.urls)),
 )
