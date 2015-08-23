d8403837041f8831724c535ca27394b3046f055c
twinsen43 <twinsen9@hotmail.com>
2015-08-23 16:10:35 +0000

    Adding path to urls

--- sb/urls.py
+++ sb/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 
-from sb.views import first_view, customer_detail, cust_viewset
+from sb.views import first_view, customer_detail, login_view
 
 urlpatterns = [
+    url(r'^login/', login_view.as_view(), name='login'),
 	url(r'^$', first_view.as_view(), name='concept_1'),
     url(r'customerDetailed/(?P<pk>\d+)/$', customer_detail.as_view(), name='customer-detail'),
 ]
